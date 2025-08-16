debc9d6edb125939e44de155c2c0d709fcc2c4ba
wolfie <ugo.costantini.1@ens.etsmtl.ca>
2025-08-16 01:12:17 -0400

    Add:
    - validation on product form
    - better formatting of License page
    - fix navigation in footer

--- src/app/features/product-nutrition-comparison/product-form/product-form.component.ts
+++ src/app/features/product-nutrition-comparison/product-form/product-form.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Product } from '../data';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-product-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.scss'
 })
@@ -25,5 +26,45 @@ export class ProductForm {
   onSubmit(): void {
     this.submitEvent.emit(this.form.value as Product);
     this.form.reset();
+  }
+
+  get price() {
+    return this.form.get('price');
+  }
+
+  get weight() {
+    return this.form.get('weight');
+  }
+
+  get servingSize() {
+    return this.form.get('servingSize');
+  }
+
+  get calories() {
+    return this.form.get('calories');
+  }
+
+  get proteins() {
+    return this.form.get('proteins');
+  }
+
+  priceInvalid(): boolean | undefined {
+    return this.price?.invalid && (this.price?.dirty || this.price?.touched)
+  }
+
+  weightInvalid(): boolean | undefined {
+    return this.weight?.invalid && (this.weight?.dirty || this.weight?.touched)
+  }
+
+  servingSizeInvalid(): boolean | undefined {
+    return this.servingSize?.invalid && (this.servingSize?.dirty || this.servingSize?.touched)
+  }
+
+  caloriesInvalid(): boolean | undefined {
+    return this.calories?.invalid && (this.calories?.dirty || this.calories?.touched)
+  }
+
+  proteinsInvalid(): boolean | undefined {
+    return this.proteins?.invalid && (this.proteins?.dirty || this.proteins?.touched)
   }
 }
